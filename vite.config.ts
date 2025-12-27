8b76a9f5338b6647cceecac02c86eade9aaf86c5
shtanriverdi <shtanriverdi@yahoo.com>
2025-12-27 19:03:37 +0300

    feat: add Tailwind CSS styles and images for kebabs and drinks; create index.html and update Vite config
    - Added Tailwind CSS styles in `index-CGvtO1Yc.css`
    - Included multiple images related to drinks and kebabs
    - Created `index.html` for the project
    - Updated Vite configuration to output build files to the `docs` directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
